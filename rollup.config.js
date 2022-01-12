e722c994311a812a7e107cfa143e3b278823a07a
Piotr Szocik <piotrek.szocik@gmail.com>
2022-01-12 18:00:40 +0100

    prod settings?

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,9 @@ import json from '@rollup/plugin-json'
 const production = !process.env.ROLLUP_WATCH
 
 // Import vercel env variables https://github.com/vercel/vercel/discussions/7016#discussioncomment-1651744
-const { SVELTE_APP_SUPABASE_URL, SVELTE_APP_SUPABASE_ANON_KEY } = process.env
+const { SVELTE_APP_SUPABASE_URL, SVELTE_APP_SUPABASE_ANON_KEY } = production
+  ? process.env
+  : config().parsed
 
 function serve() {
   let server
@@ -51,7 +53,7 @@ export default {
       __api: JSON.stringify({
         env: {
           isProd: production,
-          ...config().parsed, // attached the .env config for local dev
+          // ...config().parsed // attached the .env config for local dev
           SVELTE_APP_SUPABASE_URL, // prod,
           SVELTE_APP_SUPABASE_ANON_KEY // prod
         }
